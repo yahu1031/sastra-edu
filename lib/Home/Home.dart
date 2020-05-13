287cac37045d859e3018f17e0a9a8c8626ea803b
yahu1031 <121003219@sastra.ac.in>
2020-05-13 23:40:02 +0530

    removed borderRadius

--- lib/Home/Home.dart
+++ lib/Home/Home.dart
@@ -185,7 +185,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
         scale: _scaleAnimation,
         child: Material(
           animationDuration: duration,
-          borderRadius: BorderRadius.all(Radius.circular(30)),
+//          borderRadius: BorderRadius.all(Radius.circular(30)),
           elevation: 8,
           color: backgroundColor,
           child: SingleChildScrollView(
@@ -390,7 +390,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
                         ),
                         Container(
                           padding: EdgeInsets.only(bottom: 50.0),
-                          height: MediaQuery.of(context).size.height - 470.0,
+                          height: MediaQuery.of(context).size.height * 3,
                           child: TabBarView(
                             controller: tabController,
                             children: [
