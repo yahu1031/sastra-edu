ceb7eb5a2306b646dbf327fc74a54a5ff806dd47
minnu <minnu>
2020-05-17 13:26:05 +0530

    updated Colors and overflows

--- lib/Profile/profile.dart
+++ lib/Profile/profile.dart
@@ -2,7 +2,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_icons/flutter_icons.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:sastra_ebooks/Services/Responsive/size_config.dart';
