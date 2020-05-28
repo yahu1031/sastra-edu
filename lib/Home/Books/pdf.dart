c2daf6dea620c84292c80a335707a08527d7c5ba
minnu <minnu>
2020-05-29 05:27:09 +0530

    viewing pdfs working

--- lib/Home/Books/pdf.dart
+++ lib/Home/Books/pdf.dart
@@ -14,6 +14,10 @@ class PdfViewerPage extends StatefulWidget {
 
 class _PdfViewerPageState extends State<PdfViewerPage> {
   String localPath;
+  int _totalPages = 1;
+  int _currentPage = 0;
+  bool pdfReady = false;
+  PDFViewController _pdfViewController;
 
   @override
   void initState() {
@@ -50,10 +54,74 @@ class _PdfViewerPageState extends State<PdfViewerPage> {
         ),
       ),
       body: localPath != null
-          ? PDFView(
-              filePath: localPath,
+          ? Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Stack(
+                  children: <Widget>[
+                    PDFView(
+                      enableSwipe: true,
+                      pageSnap: true,
+                      swipeHorizontal: true,
+                      filePath: localPath,
+                      onError: (e) {
+                        print(e);
+                      },
+                      onRender: (_pages) {
+                        setState(() {
+                          _totalPages = _pages;
+                          pdfReady = true;
+                        });
+                      },
+                      onViewCreated: (PDFViewController vc) {
+                        _pdfViewController = vc;
+                      },
+                      onPageChanged: (int page, int total) {
+                        setState(() {});
+                      },
+                      onPageError: (page, e) {},
+                    ),
+                  ],
+                ),
+              ),
             )
           : Center(child: CircularProgressIndicator()),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          _currentPage > 0
+              ? Padding(
+                  padding: const EdgeInsets.only(left: 32.0),
+                  child: FloatingActionButton.extended(
+                    backgroundColor: Colors.lightBlueAccent,
+                    label: Icon(
+                      Icons.arrow_back_ios,
+                      size: 10.0,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      _currentPage -= 1;
+                      _pdfViewController.setPage(_currentPage);
+                    },
+                  ),
+                )
+              : Offstage(),
+          _currentPage + 1 < _totalPages
+              ? FloatingActionButton.extended(
+                  backgroundColor: Colors.lightBlueAccent,
+                  label: Icon(
+                    Icons.arrow_forward_ios,
+                    size: 10.0,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    _currentPage += 1;
+                    _pdfViewController.setPage(_currentPage);
+                  },
+                )
+              : Offstage(),
+        ],
+      ),
     );
   }
 }
