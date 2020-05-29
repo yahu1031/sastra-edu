0f1a6005f490d3f964c991508725b96044155631
minnu <minnu>
2020-05-30 02:22:47 +0530

    error in yourself.dart

--- lib/Profile/yourself.dart
+++ lib/Profile/yourself.dart
@@ -19,8 +19,22 @@ class YourSelf extends StatefulWidget {
 class _YourSelfState extends State<YourSelf> {
   File _image;
   bool isProfileUpdated = false;
+  String imageUrl;
+  User user = Provider.of<User>(context);
+  @override
+  void initState() async {
+    imageUrl = (await Firestore.instance.document('Data/${user.uid}').get())
+        .data['pro_pic']
+        .toString();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+
+
+
     Future getImage() async {
       var image = await ImagePicker.pickImage(source: ImageSource.gallery);
 
@@ -31,18 +45,29 @@ class _YourSelfState extends State<YourSelf> {
     }
 
     Future uploadPic(BuildContext context) async {
+      String proPicUrl;
       String fileName = basename(_image.path);
       StorageReference firebaseStorageRef =
           FirebaseStorage.instance.ref().child(fileName);
       StorageUploadTask uploadTask = firebaseStorageRef.putFile(_image);
-      StorageTaskSnapshot taskSnapshot = await uploadTask
-          .onComplete; // I don't know why this gives me a suggestion
+      StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
+      if (taskSnapshot.error == null) {
+        String dwurl = await taskSnapshot.ref.getDownloadURL();
+        print(dwurl);
+        Firestore.instance
+            .document("Data/${user.uid}")
+            .updateData({"pro_pic": dwurl});
+        proPicUrl =
+            (await Firestore.instance.document('Data/${user.uid}').get())
+                .data['pro_pic']
+                .toString();
+      }
       setState(() {
         print("Profile Picture uploaded");
+        imageUrl = proPicUrl;
       });
     }
 
-    User user = Provider.of<User>(context);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: new AppBar(
@@ -122,7 +147,7 @@ class _YourSelfState extends State<YourSelf> {
                                 fit: BoxFit.fill,
                               )
                             : Image.network(
-                                "https://images.unsplash.com/photo-1502164980785-f8aa41d53611?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                                imageUrl == null ? "" : imageUrl,
                                 fit: BoxFit.fill,
                               ),
                       ),
@@ -156,7 +181,7 @@ class _YourSelfState extends State<YourSelf> {
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.center,
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          textDirection: TextDirection.ltr,
+//                          textDirection: TextDirection.ltr,
                           children: [
                             InkWell(
                               onTap: () {
